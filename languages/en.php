0b7586376b7eb27c0b42fd99834f61503acbaf1b
Jeff Tilson <jrtilson@gmail.com>
2010-07-19 18:22:34 +0000

    Plugin directories and files added

---
+++ languages/en.php
@@ -0,0 +1,38 @@
+<?php
+	/**
+	 * Parent portal language file
+	 * 
+	 * @package ParentPortal
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Jeff Tilson
+	 * @copyright THINK Global School 2010
+	 * @link http://www.thinkglobalschool.com/
+	 * 
+	 */
+	
+	$english = array(
+		
+		// Entity/Object Related
+		'parentportal' => 'Parent Portal',
+		
+		// Default Content
+		
+		// Menu
+
+		// Actions
+		
+		// Action labels
+	
+		// Confirmations
+	
+		// Errors
+			
+		// Titles/Label
+
+		// Other
+		
+	
+	);
+
+	add_translation('en',$english);
+?>
---
+++ start.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * Parent portal start.php
+	 * 
+	 * @package ParentPortal
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Jeff Tilson
+	 * @copyright THINK Global School 2010
+	 * @link http://www.thinkglobalschool.com/
+	 * 
+	 */
+	
+	// Init
+	function parentportal_init() {
+
+	
+	}
+
+	register_elgg_event_handler('init', 'system', 'parentportal_init');
+?>
