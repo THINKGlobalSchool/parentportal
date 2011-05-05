f9b7443418b52c52a22d06bb228036cef096ef60
Jeff Tilson <jrtilson@gmail.com>
2011-05-05 19:19:57 +0000

    Fixed value checking. The custom module now essentially supports grabbing any combination of entities with container_guid, a tag, and subtype(s).

--- lib/ajaxmodule.php
+++ lib/ajaxmodule.php
@@ -12,34 +12,49 @@
 
 function am_list_entities_by_group_or_tag($options) {
 	
-	// Get and check vars
-	$container_guid = ($options['container_guid'] === NULL) ? 0 : $options['container_guid'];
-	$tag			= ($options['tag'] === NULL) ? '' : $options['tag'];
+	$false_sql = "FALSE";
+	
+	if ($options['container_guid']) {
+		$container_sql = "e.container_guid IN ({$options['container_guid']})";
+	} else {
+		$container_sql = "FALSE";
+	}
+	
+	if ($options['tag']) {
+		$access_sql = get_access_sql_suffix('tag_meta_table');
+		$tag_sql = "
+			(
+				(tag_msn.string IN ('tags')) AND ( BINARY tag_msv.string IN ('{$options['tag']}')) 
+				AND
+				$access_sql
+			)
+		";
+	} else {
+		$tag_sql = "FALSE";
+	}
+		
 	$subtypes		= (is_array($options['subtypes'])) ? $options['subtypes'] : array();
 	$limit			= ($options['limit'] === NULL) ? 10 : $options['limit']; 
 	$offset			= ($options['offset'] === NULL) ? 0 : $options['offset'];
 	$title 			= ($options['title'] === NULL) ? 'Custom Module' : $options['title'];
-
+	
 	global $CONFIG;
 
 	$joins[] = "JOIN {$CONFIG->dbprefix}metadata tag_meta_table on e.guid = tag_meta_table.entity_guid";
 	$joins[] = "JOIN {$CONFIG->dbprefix}metastrings tag_msn on tag_meta_table.name_id = tag_msn.id";
 	$joins[] = "JOIN {$CONFIG->dbprefix}metastrings tag_msv on tag_meta_table.value_id = tag_msv.id";
 
-	$access_sql = get_access_sql_suffix('tag_meta_table');
-
-	// Need to watch the brackets here.. 
-	$wheres[] = "
-		(
-			e.container_guid IN ({$container_guid})
-			OR
+	// As long as we have either a container_guid or a tag, use the $wheres
+	if ($container_sql != $false_sql || $tag_sql != $false_sql) {
+		// Need to watch the brackets here.. 
+		$wheres[] = "
 			(
-				(tag_msn.string IN ('tags')) AND ( BINARY tag_msv.string IN ('{$tag}')) 
-				AND
-				$access_sql
+				$container_sql
+				OR
+				$tag_sql
 			)
-		)
-	";
+		";
+	}
 	
 	set_context('search');
 
