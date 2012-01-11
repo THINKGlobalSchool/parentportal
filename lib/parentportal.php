0ecccef8f20d9761e724967a7a61c6d4fc84b02c
Jeff Tilson <jrtilson@gmail.com>
2012-01-11 01:18:49 +0000

    Fix deprecated code

--- lib/parentportal.php
+++ lib/parentportal.php
@@ -230,7 +230,7 @@ function parentportal_get_page_content_user_settings() {
 
 	// Make sure we don't open a security hole ...
 	if ((!elgg_get_page_owner_entity()) || (!elgg_get_page_owner_entity()->canEdit())) {
-		set_page_owner(elgg_get_logged_in_user_guid());
+		elgg_set_page_owner_guid(elgg_get_logged_in_user_guid());
 	}
 	
 	$params['title'] = elgg_echo('parentportal:title:usersettings');
